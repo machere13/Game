f933e4fe07056c7be61d18e3fb8d675844b5db4f
machere13 <vladislav.chubarov13@yandex.ru>
2026-06-19 14:15:35 +0300

    tune: nudge Рынок right, Набережная up-right

--- Assets/Scripts/Prototypes/PancakeFlip/Editor/PancakeFlipSetup.cs
+++ Assets/Scripts/Prototypes/PancakeFlip/Editor/PancakeFlipSetup.cs
@@ -199,9 +199,9 @@ namespace IdlePancake.Prototypes.PancakeFlip.Editor
             locStall.requiredLevel = 1; locStall.cityCost = 0;
             locStall.mapPosition = new Vector2(0.64f, 0.16f); locStall.mapIcon = city01Spr;
             locPromenade.requiredLevel = 3; locPromenade.cityCost = 150;
-            locPromenade.mapPosition = new Vector2(0.44f, 0.42f); locPromenade.mapIcon = city02Spr;
+            locPromenade.mapPosition = new Vector2(0.49f, 0.47f); locPromenade.mapIcon = city02Spr;
             locMarket.requiredLevel = 5; locMarket.cityCost = 300;
-            locMarket.mapPosition = new Vector2(0.82f, 0.44f); locMarket.mapIcon = city03Spr;
+            locMarket.mapPosition = new Vector2(0.87f, 0.44f); locMarket.mapIcon = city03Spr;
             EditorUtility.SetDirty(locStall); EditorUtility.SetDirty(locPromenade); EditorUtility.SetDirty(locMarket);
 
             var worldMap = GetOrCreate<WorldMapConfig>(DataDir, "WorldMap");
